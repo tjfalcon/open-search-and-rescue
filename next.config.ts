9ec73da02b3adecc737cdf3451fa93e1f5f1a2ec
Thomas Falcon <thomas.jfalcon@gmail.com>
2025-09-18 17:09:07 -0600

    feat: add maintenance mode

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    MAINTENANCE_MODE: process.env.MAINTENANCE_MODE,
+    MAINTENANCE_BYPASS_TOKEN: process.env.MAINTENANCE_BYPASS_TOKEN,
+  },
 };
 
 export default nextConfig;
